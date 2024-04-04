6545d6fa7fcae96ae0abca184c87e22f792062c7
MarioVM <mario.medeiros@sou.inteli.edu.br>
2024-04-04 16:47:49 -0300

    correcao de erros do controle do robô e inclusao de log automatico

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify, render_template, render_template_string
+from flask import Flask, request, jsonify, render_template, render_template_string, redirect
 from flask_cors import CORS
 from robo import InteliArm
 from serial.tools import list_ports
 from tinydb import TinyDB, Query
 import os
+from datetime import datetime
 
 app = Flask(__name__, template_folder='../templates')
 CORS(app)
@@ -60,8 +61,13 @@ def connect():
         if Robo.conectar_porta(port):
             # If the connection is successful, you can return success message or the next steps
             return render_template_string("""
-                <div>Conectado ao robô com sucesso!</div>
-                <!-- Your success logic here -->
+                    <form action="/movej" method="post"><br>
+                        <label>X: </label><input type="number" id="xValue" name="x" value="0"><br>
+                        <label>Y: </label><input type="number" id="yValue" name="y" value="0"><br>
+                        <label>Z: </label><input type="number" id="zValue" name="z" value="0"><br>
+                        <label>R: </label><input type="number" id="rValue" name="r" value="0"><br>
+                        <input type="submit" value="enviar">
+                    </form>
             """)
         else:
             # If conectar_porta returns False, it means the connection attempt failed
@@ -76,13 +82,31 @@ def connect():
 
 @app.route('/movej', methods=['POST'])
 def movej():
-    data = request.json
+    data = request.form
     x = float(data.get('x', 0))
     y = float(data.get('y', 0))
     z = float(data.get('z', 0))
     r = float(data.get('r', 0))
+
+    # Execute the robot move command
     Robo.movej_to(x, y, z, r)
-    return jsonify({"message": "Moved using movej"})
+
+    # Get the current datetime
+    now = datetime.now()
+
+    # Create a record to insert into the database
+    log = {
+        'x': x,
+        'y': y,
+        'z': z,
+        'r': r,
+        'datetime': now.strftime("%Y-%m-%d %H:%M:%S")  # Format datetime as string
+    }
+
+    # Insert the record into the database
+    db.insert(log)
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
